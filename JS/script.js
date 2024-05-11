73dc85f5d92f956fd14cb3cc6bb09a2964226672
Pablo <pablonietoperez369@gmail.com>
2024-05-11 02:46:48 -0500

    Mensaje descriptivo de los cambios

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,25 @@
-$(document).ready(function(){
+$(function(){
     setTimeout(function(){
-        $("#video").addClass("testVideo");
+        $("#video").addClass("animationVideo1");
     }, 1000);
 });
+
+$(function(){
+    setTimeout(function(){
+        $("#headerContent").addClass("navBarAnimation");
+    }, 500);
+});
+
+$(function(){
+	$(".Menu").click(function(e){
+		$(".overlayMenu").toggleClass("overlayMenuOpen");
+		$(".backVid").toggleClass("animationVideo2");
+	});
+});
+
+$(function(){
+	$(".sectionBackground").hover(function(e){
+		$(".sectionBackground").toggleClass("backgroundAnimation");
+	});
+});
+
