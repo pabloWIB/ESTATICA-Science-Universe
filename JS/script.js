162af9e968fe5b62707a92c2cb8920860c0a4d5e
Pablo <pablonietoperez369@gmail.com>
2024-05-12 18:26:48 -0500

    Página terminada

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,14 @@
 $(function(){
     setTimeout(function(){
         $("#video").addClass("animationVideo1");
-    }, 1000);
+		$(".footerContent").addClass("mainContentrAnimation");
+    }, 700);
 });
 
 $(function(){
     setTimeout(function(){
         $("#headerContent").addClass("navBarAnimation");
+		$(".mainContent").addClass("mainContentrAnimation");
     }, 500);
 });
 
@@ -14,12 +16,19 @@ $(function(){
 	$(".Menu").click(function(e){
 		$(".overlayMenu").toggleClass("overlayMenuOpen");
 		$(".backVid").toggleClass("animationVideo2");
+		$("body").toggleClass("bodyOverflowAnimation");
 	});
 });
 
 $(function(){
 	$(".sectionBackground").hover(function(e){
 		$(".sectionBackground").toggleClass("backgroundAnimation");
+	});
+});
+
+$(function(){
+	$(".findArrowUnderline").hover(function(e){
+		$(".textArea2").toggleClass("pene");
 	});
 });
 
